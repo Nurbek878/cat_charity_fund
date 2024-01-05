49df8b91aae168a9e8e17d971af5d93136249c36
Nurbek878 <nurik878878@yandex.ru>
2024-01-05 20:03:15 +0500

    Create endpoints, validators, routers

--- app/api/endpoints/charityproject.py
+++ app/api/endpoints/charityproject.py
@@ -1,14 +1,13 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.db import get_async_session
 
 from app.crud.charityproject import charityproject_crud
-from app.models.charityproject import CharityProject
+from app.api.validators import check_name_duplicate, check_charityproject_exists
 
 from app.schemas.charityproject import CharityProjectCreate, CharityProjectDB, CharityProjectUpdate
 
-router = APIRouter(prefix='/charity_project',
-                   tags=['charity_projects'])
+router = APIRouter()
 
 
 @router.post('/',
@@ -71,31 +70,4 @@ async def remove_charityproject(
     charityproject = await charityproject_crud.remove(
         charityproject, session
     )
-    return charityproject
-
-
-async def check_name_duplicate(
-        charityproject_name: str,
-        session: AsyncSession,
-) -> None:
-    charityproject_id = await charityproject_crud.get_charityproject_id_by_name(charityproject_name, session)
-    if charityproject_id is not None:
-        raise HTTPException(
-            status_code=422,
-            detail='Проект с таким именем уже существует!',
-        )
-
-
-async def check_charityproject_exists(
-        charityproject_id: int,
-        session: AsyncSession,
-) -> CharityProject:
-    charityproject = await charityproject_crud.get(
-        charityproject_id, session
-    )
-    if charityproject is None:
-        raise HTTPException(
-            status_code=404,
-            detail='Проект не найден!'
-        )
     return charityproject
---
+++ app/api/routers.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+
+from app.api.endpoints import charityproject_router, donation_router
+
+
+main_router = APIRouter()
+main_router.include_router(charityproject_router, prefix='/charity_project',
+                           tags=['charity_projects'])
+main_router.include_router(donation_router, prefix='/donation',
+                           tags=['donations'])
